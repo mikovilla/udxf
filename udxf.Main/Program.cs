20be78ab5fb86c313e46557c57d695d17322f67c
dev@mikovilla.com <dev@mikovilla.com>
2025-05-01 01:54:43 -0700

    add benchmark

---
+++ udxf.Main/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Diagnostics;
+using udxf.Utility;
+
+var numberOfEntries = new[] { 10, 100, 1_000, 10_000, 100_000, 1_000_000 };
+
+Stopwatch sw = Stopwatch.StartNew();
+foreach (var entry in numberOfEntries)
+{
+    var json = RandomGenerator.CreateJson(entry);
+    var xml = json.Reformat();
+    Console.WriteLine($"Json ({json.Length} characters) with {entry} objects serialized to Xml ({xml.Length}) for {sw.ElapsedMilliseconds} ms");
+}
+sw.Stop();
+
+Console.WriteLine();
+
+sw = Stopwatch.StartNew();
+foreach (var entry in numberOfEntries)
+{
+    var xml = RandomGenerator.CreateJson(entry);
+    var json = xml.Reformat();
+    Console.WriteLine($"Xml ({xml.Length} characters) with {entry} objects serialized to Json ({json.Length}) for {sw.ElapsedMilliseconds} ms");
+}
+sw.Stop();
---
+++ udxf.Utility/RandomGenerator.cs
@@ -0,0 +1,51 @@
+﻿using System.Text.Json;
+using System.Xml.Linq;
+
+namespace udxf.Utility
+{
+    public static class RandomGenerator
+    {
+        public static string CreateJson(int numberOfEntries)
+        {
+            var random = new Random();
+            var jsonDict = new Dictionary<string, object>();
+
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+            for (int i = 0; i < numberOfEntries; i++)
+            {
+                string randomKey = new string(Enumerable.Repeat(chars, 6)
+                    .Select(s => s[random.Next(s.Length)]).ToArray());
+
+                object randomValue = random.Next(2) == 0
+                    ? (object)random.Next(1, 100) 
+                    : (object)$"Value{random.Next(1, 100)}"; 
+
+                jsonDict[randomKey] = randomValue;
+            }
+            return JsonSerializer.Serialize(jsonDict, new JsonSerializerOptions { WriteIndented = true });
+        }
+
+        public static string CreateXml(int numberOfEntries)
+        {
+            var random = new Random();
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+            XElement root = new XElement("Root");
+
+            for (int i = 0; i < numberOfEntries; i++)
+            {
+                string randomKey = new string(Enumerable.Repeat(chars, 6)
+                    .Select(s => s[random.Next(s.Length)]).ToArray());
+
+                string randomValue = random.Next(2) == 0
+                    ? random.Next(1, 100).ToString() 
+                    : $"Value{random.Next(1, 100)}"; 
+
+                root.Add(new XElement(randomKey, randomValue));
+            }
+
+            return root.ToString();
+        }
+    }
+}
